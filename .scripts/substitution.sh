46b3b25c850c56f4fa5249503d41b2fc75a605b2
Cor77 <corleone77daniel@gmail.com>
2023-05-29 16:05:21 +0200

    new proof x1

--- .scripts/substitution.sh
+++ .scripts/substitution.sh
@@ -4,11 +4,8 @@ SOURCE=$1
 TARGET=$2
 
 for i in `grep -ir  "$SOURCE" ./replacement/* | awk '{print $1}'| cut -d ":" -f1`; do
-	echo $SOURCE
-	echo $TARGET
-	echo $i
 
 	sed -i "s/$SOURCE/$TARGET/" $i
-	cat ./replacement/*
+	echo $i
 	
 done
