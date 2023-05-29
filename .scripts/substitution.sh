1ec887ebd5acc04744691245ad17fe9eff86acfe
Cor77 <corleone77daniel@gmail.com>
2023-05-29 15:17:52 +0200

    new changes script

--- .scripts/substitution.sh
+++ .scripts/substitution.sh
@@ -6,8 +6,9 @@ TARGET=$2
 for i in `grep -ir  "$SOURCE" ./replacement/* | awk '{print $1}'| cut -d ":" -f1`; do
 	echo $SOURCE
 	echo $TARGET
+	echo $i
 
-	sed -i "s/$SOURCE/$TARGET/" $i
+	sed -i "s/${SOURCE}/{$TARGET}/" $i
 
 	echo "Variable Replaced"
 	
